d6bf9fa68b7f661e40fe3567efefa5b2345e7197
Lappland <43505649+aventador917@users.noreply.github.com>
2019-09-28 09:59:55 -0700

    Add files via upload

---
+++ Assignment#1/main.cpp
@@ -0,0 +1,240 @@
+/*
+Paul Tang
+
+CPSC 323
+September 28, 2019
+
+Assignment#1
+*/
+
+#include<string>
+#include<stdlib.h>
+#include<stdio.h>
+#include<iostream>
+#include <fstream>
+
+using namespace std;
+
+bool isLetter(char ch) {//identifier
+	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isDigit(char ch) {//isDigit 
+	if (ch >= '0' && ch <= '9')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isOperators(char ch) {// isOperators 
+	if (ch == '+' || ch == '*' || ch == '-' || ch == '/' || ch == '=' || ch == ':' || ch == '<' || ch == '>')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool isDelimiter(char ch) 
+{// is separator
+	if (ch == ',' || ch == ';' || ch == '.' || ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '#' || ch == '\\'||ch == '"' || ch == '\'')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool isBlank(char ch) {
+	if (ch == ' ' || ch == '\t')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+char  key[34][10] = { "main",
+"auto", "short", "int", "long", "float", "double", "char", "struct"
+, "union", "enum", "typedef", "const", "unsigned", "signed", "extern", "register"
+, "static", "volatile", "void", "if", "else", "switch", "case", "for"
+, "do", "while", "goto", "continue", "break", "default", "sizeof", "return","then" };
+
+void fsm(char sourceCode[256])
+{
+	ofstream out("out.txt");
+	int number = 0;
+	int h = 0;
+	char hh[100][10] = { '\0' };
+	out << "token            lexeme" << endl;
+	out << "-----------------------" << endl;
+	for (int i = 0; i < 256; i) {
+		//char delimiter[2] = "";
+		int j = 0;  //letter  
+		int l = 0;   //digit 
+		int k = 0;   //operators  
+
+		int b = 0;  // The typerNumber; the identifier is 1,real number is 2, keyword is 3, operator is 4 and separator is 5.
+		char num[100] = "";
+		char word[100] = "";
+		if (isBlank(sourceCode[i]) == 1) 
+		{
+			i += 1;
+		}//remove whitespace
+		else {
+			if (isLetter(sourceCode[i]) == 1)         //identifier
+			{                                             //yes
+				do
+				{
+					if (j < 10) 
+					{
+						word[j] = sourceCode[i];
+						j++;
+					}
+				} while (isDigit(sourceCode[++i]) == 1 || isLetter(sourceCode[i]) == 1);
+
+				int a = 0;
+				while (a < 34) 
+				{
+					if (strcmp(key[a], word) == 0) 
+					{
+						b = 3;
+						a++;
+						break;
+					}
+					else 
+					{
+						b = 1;
+						a++;
+						
+					}
+				}
+				if (b == 3)
+				{
+					out << "keyword          " << word << endl;
+				}
+				if (b == 1) 
+				{
+					int q = 0, qq = 0;
+					for (int qq = 0; strcmp(hh[qq], "") != 0; qq++) 
+					{
+						if (strcmp(word, hh[qq]) == 0) 
+						{
+							q = 1; break;
+						}
+					}
+					if (q == 1) 
+					{
+					out << "identifier          " <<word<< endl;
+						q = 0;
+					}
+					else 
+					{
+						for (qq = 0; qq < j; qq++) 
+						{
+							hh[h][qq] = word[qq];
+						}
+					out << "identifier          " << word<< endl;
+						h++;
+					}
+				}
+				//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			}
+			else if (isDigit(sourceCode[i]) == 1)//realnumber
+			{
+				int c = 0;
+				while (isDigit(sourceCode[i]) || sourceCode[i] == '.' ) 
+				{
+
+					num[c] = sourceCode[i];
+					i += 1;
+					c++;
+				}
+				out << "real               " << num << endl;
+			}
+			else if (isDelimiter(sourceCode[i]) == 1) //separator
+			{
+
+				out << "separator          " << sourceCode[i] << endl;
+				i++;
+			}
+			else if (isOperators(sourceCode[i]) == 1) //operator
+			{
+
+				if (sourceCode[i] == ':' || sourceCode[i] == '>' || sourceCode[i] == '<'&&sourceCode[i + 1] == '=') 
+				{
+					out << "operator          " << sourceCode[i]<<sourceCode[i+1] << endl;
+					i += 2;
+				}
+				else 
+				{
+					out << "operator          " << sourceCode[i] << endl;
+					i += 1;
+				}
+			}
+			else 
+			{
+				if (sourceCode[i] != '\0')
+				{
+					out << "error         " << sourceCode[i] << endl;
+					
+				}
+				if (sourceCode[i + 1] != '\0') 
+				{
+
+					i += 1;
+				}
+				else
+				{
+					i = 256;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	string fileName;
+	char originCode[256] = "";
+	
+	cout << "TestCase1.txt" << endl;
+	cout << "TestCase2.txt" << endl;
+	cout << "TestCase3.txt" << endl << endl;
+
+	cout << "Please input the file you want to test: ";
+	cin >> fileName;
+	cout << endl;
+	
+
+	fstream outFile;
+
+	outFile.open(fileName, ios::in);
+	
+	while (!outFile.eof())
+	{
+		outFile.getline(originCode, 256,'\n');
+	}
+	outFile.close();
+
+
+	fsm(originCode);
+	
+	cout << "See the result in output.txt" << endl;
+	system("pause");
+	return 0;
+}
